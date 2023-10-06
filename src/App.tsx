e8af3eeca2259254ae2b0fa3317969bf25ed904c
Kibreab Aklilu <Akkeba37@gmail.com>
2023-10-06 17:23:03 +0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-slate-800 text-5xl font-bold ">Hey</h1>
+      <button className="btn btn-secondary">One</button>
+    </>
+  );
+}
+
+export default App;
