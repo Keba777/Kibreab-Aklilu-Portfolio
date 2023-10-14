45a15b87b7e6cc73a482d326e8db04bd47d5b1b0
Kibreab Aklilu <Akkeba37@gmail.com>
2023-10-14 20:09:09 +0300

    Build Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
+import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <NavBar />
-      <div className="md:grid grid-flow-col ">
-        <aside className="bg-slate-600">aside</aside>
-        <main className="bg-sky-400">main</main>
-      </div>
+      <main>
+        <Hero />
+      </main>
     </>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,35 @@
+import Profile from "../assets/profile.jpg";
+
+const Hero = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200 px-28">
+      <div className="hero-content flex-col lg:flex-row">
+        <img
+          src={Profile}
+          alt="Profile Picture"
+          className="max-w-xs rounded-lg shadow-2xl mr-6"
+        />
+
+        <div>
+          <h2 className="text-5xl  font-bold mb-4">Kibreab Aklilu</h2>
+          <h2 className="text-3xl  font-bold">"Full Stack Web Developer"</h2>
+          <p className="py-6 text-justify">
+            Hello! I'm a passionate Full Stack Web Developer, turning ideas into
+            extraordinary web applications using React.js, Next.js, Node.js and
+            other technologies. In the realm of coding, I craft seamless and
+            unforgettable digital experiences. My expertise goes beyond
+            functionality—I'm all about creating interfaces that leave a lasting
+            impression.
+          </p>
+          <p className="text-justify">
+            My journey at A2SV has refined my skills and honed my ability to
+            deliver high-quality web solutions. Let's innovate, create, and
+            enhance the web, one line of code at a time!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
